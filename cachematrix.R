bc06bbfca360db1202e23eb2c5dfaf51c226aec6
gimagarotto <69356217+gimagarotto@users.noreply.github.com>
2023-03-20 20:48:51 +0100

    Update cachematrix.R
    Cursera R Programming curse, Programming Assignment 2 - Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+  solved <- NULL
+  set <- function(y) {
+    x <<- y
+    solved <<- NULL
+  }
+  get <- function() x
+  setsolved <- function(solve) solved <<- solve
+  getsolved <- function() solved
+  list(set = set, get = get,
+       setsolved = setsolved,
+       getsolved = getsolved)
+} ###creates an inversible matrix based on an original matrix 
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  solved <- x$getsolved()
+  if(!is.null(solved)) {
+    message("getting inverse matrix")
+    return(solved)
+  }
+  data <- x$get()
+  solved <- solve(data, ...)
+  x$setsolved(solved)
+  solved ## Return a matrix that is the inverse of 'x'- the inversible matrix
 }
+
+
+###testing 
+matriz<- matrix(rnorm(4),2,2)
+matrizInv <- makeCacheMatrix(matriz)
+cacheSolve(matrizInv)
